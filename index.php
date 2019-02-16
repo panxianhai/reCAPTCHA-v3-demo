999195fcff7ebb41ee0f9dc13ac14663ad60c9c2
holst <truhin.pavel@gmail.com>
2019-02-16 22:21:20 +0200

    change project

--- index.php
+++ index.php
@@ -2,31 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>reCaptcha V3</title>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js"></script>
-    <script src='https://www.recaptcha.net/recaptcha/api.js?render=6Lf4JHgUAAAAAOQsWq7eXzUc6kLDpyf5OO1cVQLL'></script>
-    <script>
-        function reCaptcha() {
-            grecaptcha.ready(function() {
-                grecaptcha.execute('6Lf4JHgUAAAAAOQsWq7eXzUc6kLDpyf5OO1cVQLL', {action: 'homepage'})
-                .then(function(token) {
-                    // Verify the token on the server.
-                    $.ajax({
-                        type: 'post',
-                        url: './server.php',
-                        data: {token: token},
-                        dataType: 'json'
-                    }).done(function(data) {
-                        alert('Score: ' + data.score);
-                    });
-                });
-            });
-        }
-    </script>
+    <title>Test</title>
 </head>
 <body>
-    <button onclick="reCaptcha()">reCaptcha V3 - t</button>
+    Test
 </body>
 </html>
